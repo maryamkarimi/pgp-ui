74a714c20bce9f46167c5aef4073523c9be9c3a0
Maryam Karimi <mkarimif@uwo.ca>
2021-01-02 21:08:52 -0500

    Added eslint to the project
    Fixed existing eslint warnings

---
+++ src/Main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './images/flowers.jpg';
+import check from './images/green-check.png';
+import redx from './images/red-x.png';
+import nextarrow from './images/next-arrow.png';
+
+const Main = ({ handleLogout }) => (
+  <section className="hero">
+    <nav>
+      <h2>Welcome</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </nav>
+
+    <div className="main">
+      <div className="image">
+        <img src={logo} width="800px" />
+      </div>
+
+      <div className="questionBox">
+        <h2>
+          This is a test, how many flowers do you see in the picture?
+          Also, is there a stem in the photo?
+        </h2>
+
+        <div className="imageAnswerBox">
+          <img src={check} width="200px" />
+          <img src={redx} width="200px" />
+          <img src={nextarrow} className="arrow" />
+        </div>
+
+        {/*
+                    <div className="formAnswerBox">
+                        <form>
+                            <label for="fname">How many flowers?</label><br/>
+                            <input type="text" id="fname" name="fname"></input><br/>
+                            <label for="lname">Is there a stem in the photo?</label><br/>
+                            <input type="text" id="lname" name="lname"></input>
+                        </form>
+                    </div>
+                    */}
+      </div>
+    </div>
+
+  </section>
+);
+
+export default Main;
--- src/fire.js
+++ src/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
-  
-var firebaseConfig = {
-    apiKey: "AIzaSyB6IuDOhzpiP39Jp-Zv6asfclXpCGhBrI4",
-    authDomain: "login-926f3.firebaseapp.com",
-    databaseURL: "https://login-926f3.firebaseio.com",
-    projectId: "login-926f3",
-    storageBucket: "login-926f3.appspot.com",
-    messagingSenderId: "181893105519",
-    appId: "1:181893105519:web:a1eae928b18f7a0e930a9a"
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB6IuDOhzpiP39Jp-Zv6asfclXpCGhBrI4',
+  authDomain: 'login-926f3.firebaseapp.com',
+  databaseURL: 'https://login-926f3.firebaseio.com',
+  projectId: 'login-926f3',
+  storageBucket: 'login-926f3.appspot.com',
+  messagingSenderId: '181893105519',
+  appId: '1:181893105519:web:a1eae928b18f7a0e930a9a',
 };
 const fire = firebase.initializeApp(firebaseConfig);
 
-export default fire; 
+export default fire;
