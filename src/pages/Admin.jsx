7cfa85cbe1843e837e16b03db59a249c27196ceb
Maryam Karimi <mkarimif@uwo.ca>
2021-01-03 21:26:20 -0500

    Added a few components/pages I'm currently working on
    Not complete yet

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Images from '../components/Images';
+import Cues from '../components/Cues';
+
+const Admin = () => {
+  return (
+    <>
+      <Images/>
+      <Cues/>
+    </>
+  );
+};
+
+export default Admin;
