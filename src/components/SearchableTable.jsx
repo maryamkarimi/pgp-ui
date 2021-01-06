3a2c7f901557402e5be0a450e112bdeb61bdc453
Maryam Karimi <mkarimif@uwo.ca>
2021-01-06 16:00:02 -0500

    - Updated some styling
    - Replaced the dropdown with a searchbox to search in the searchable table
    
    - Fixed a bug where newly added items weren't being displayed

--- src/components/SearchableTable.jsx
+++ src/components/SearchableTable.jsx
@@ -1,43 +1,18 @@
-import React, { useState } from 'react';
-import { Button, Table, Input, Space, Popconfirm } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Table, Input, Popconfirm } from 'antd';
 import Highlighter from 'react-highlight-words';
 import { DeleteOutlined, SearchOutlined } from '@ant-design/icons';
 import './SearchableTable.css';
 
 const SearchableTable = ({ title, dataSource, searchIndex, handleDelete }) => {
   const [searchText, setSearchText] = useState('');
+  const [filteredData, setFilteredData] = useState();
+
+  useEffect(() => {
+    setFilteredData(filterData(dataSource, searchText));
+  }, [dataSource]);
 
   const getColumnSearchProps = () => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-      <div className="filterDropDown">
-        <Input
-          className="search-input"
-          placeholder={`Search`}
-          value={selectedKeys[0]}
-          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-          onPressEnter={() => handleSearch(selectedKeys, confirm)}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm)}
-            icon={<SearchOutlined />}
-            size="small"
-          >
-              Search
-          </Button>
-          <Button onClick={() => handleReset(clearFilters)} size="small">
-              Reset
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered) =>
-      <SearchOutlined style={{ color: filtered ? '#7829ca' : '#bfbfbf' }} />,
-    onFilter: (value, record) =>
-      record[searchIndex] ?
-          record[searchIndex].toString().toLowerCase().includes(value.toLowerCase()) :
-          '',
     render: (record) =>
       <div className="table-row">
         <Highlighter
@@ -47,7 +22,7 @@ const SearchableTable = ({ title, dataSource, searchIndex, handleDelete }) => {
           textToHighlight={record[searchIndex] ? record[searchIndex].toString() : ''}
         />
         <Popconfirm
-          title="Are you sure to delete this cue?"
+          title="Are you sure to delete this item?"
           onConfirm={() => handleDelete(record)}>
           <Button type="link" icon={<DeleteOutlined style={{ color: '#bfbfbf' }}/>} />
         </Popconfirm>
@@ -55,25 +30,42 @@ const SearchableTable = ({ title, dataSource, searchIndex, handleDelete }) => {
     ,
   });
 
-  const handleSearch = (selectedKeys, confirm) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
+
+  const filterData = (data, text) => {
+    if (text === '') return data;
+    return data.filter(
+        (record) => record[searchIndex].toString().toLowerCase().includes(text),
+    );
   };
 
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText('' );
+  const handleSearch = (e) => {
+    const filtered = filterData(dataSource, e.target.value);
+
+    setSearchText(e.target.value);
+    setFilteredData(filtered);
   };
+
+  const titleRow =
+      <div className="table-row">
+        { title }
+        <Input
+          placeholder="Search..."
+          onChange={handleSearch}
+          style={{ width: 200 }}
+          prefix={<SearchOutlined/>}
+          allowClear
+        />
+      </div>;
 
   const columns = [
     {
-      title: title,
+      title: titleRow,
       ...getColumnSearchProps(),
     },
   ];
 
   return (
-    <Table columns={columns} dataSource={dataSource} size='small'/>
+    <Table className="searchable-table" columns={columns} dataSource={filteredData} size='small'/>
   );
 };
 
