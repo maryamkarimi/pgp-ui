572f1552a02b5bd7d96b1f4df70603578055feda
Nicholas Tocco <ntocco@ibm.com>
2021-02-14 02:02:51 -0500

    add s3 upload functionality

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   s3: {
-    REGION: 'YOUR_S3_UPLOADS_BUCKET_REGION',
-    BUCKET: 'YOUR_S3_UPLOADS_BUCKET_NAME',
+    REGION: 'us-east-2',
+    BUCKET: 'myporngenomebucket',
   },
   apiGateway: {
     NAME: 'pgp',
