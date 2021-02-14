e541bab081c90ab91395543f839f66e6f3ffe545
Nicholas Tocco <ntocco@ibm.com>
2021-02-14 02:02:51 -0500

    add s3 upload functionality

--- src/index.js
+++ src/index.js
@@ -4,31 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import config from './config';
+import awsconfig from './aws-exports';
 
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-  },
-  Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-  },
-  API: {
-    endpoints: [
-      {
-        name: config.apiGateway.NAME,
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION,
-      },
-    ],
-  },
-});
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
     <React.StrictMode>
