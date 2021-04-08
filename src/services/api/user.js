0fb4e418992cf1ca131c22061aa34a609329e126
Maryam Karimi <mkarimif@uwo.ca>
2021-04-07 21:22:42 -0400

    Fixed the bug in signUp user

--- src/services/api/user.js
+++ src/services/api/user.js
@@ -2,6 +2,7 @@ import { getApiName } from '../index';
 import { API } from 'aws-amplify';
 
 export const signUpUser = (userInfo) => {
+  const body = { ...userInfo };
   return API
-      .post(getApiName(), '/user/new-user', { ...userInfo });
+      .post(getApiName(), '/user/new-user', { body });
 };
